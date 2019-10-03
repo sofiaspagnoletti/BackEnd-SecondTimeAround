002c342eae66d4394ec38b1075f4f0e088d44107
sofi.spag <sofi.spag@gmail.com>
2019-10-03 11:32:19 -0700

    creating database

---
+++ models/post.js
@@ -0,0 +1,63 @@
+module.exports = function (sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    product: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    },
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    apt: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        len: [1]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    zipcode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    }
+  });
+
+  Post.associate = function (models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Post.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Post;
+};
---
+++ routes/user-api-routes.js
@@ -0,0 +1,45 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/users", function(req, res) {
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.User.findAll({
+      include: [db.Post]
+    }).then(function(dbUSer) {
+      res.json(dbUSer);
+    });
+  });
+
+  app.get("/api/users/:id", function(req, res) {
+    // Here we add an "include" property to our options in our findOne query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Post
+    db.User.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.Post]
+    }).then(function(dbUSer) {
+      res.json(dbUSer);
+    });
+  });
+
+  app.post("/api/users", function(req, res) {
+    db.User.create(req.body).then(function(dbUSer) {
+      res.json(dbUSer);
+    });
+  });
+
+  app.delete("/api/users/:id", function(req, res) {
+    db.User.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbUSer) {
+      res.json(dbUSer);
+    });
+  });
+
+};
